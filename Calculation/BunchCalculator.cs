5188981c9e26eceb92c103e14ab21f67b34a8402
RomanDubinin <romandubinin94@gmail.com>
2021-01-08 13:31:23 +0100

    bunchCalculator

---
+++ Calculation/BunchCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Calculation
+{
+    public class BunchCalculator
+    {
+        private Calculator calculator;
+
+        public BunchCalculator(Calculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public IEnumerable<string> Calculate(IEnumerable<string> expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                var result = calculator.CalculateFromString(expression);
+
+                if (!result.Validation.IsSuccess)
+                    yield return result.Validation.ErrorMessage;
+                else if (result.CalculationError != null)
+                    yield return result.CalculationError;
+                else
+                    yield return result.Result.ToString();
+            }
+        }
+    }
+}
---
+++ CalculatorUnitTests/BunchCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Calculation;
+using NUnit.Framework;
+
+namespace CalculatorUnitTests
+{
+    public class BunchCalculatorTests
+    {
+        private BunchCalculator bunchCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            bunchCalculator = new BunchCalculator(new Calculator());
+        }
+
+        [Test]
+        public void FileCalculationTest()
+        {
+            var lines = new[] {"1+2", "1+", "999999999999999999999999999999999999999+1"};
+            var expectedResults = new[] {"3", "Value missed at position 1", "Too large numbers"};
+            var actualResults = bunchCalculator.Calculate(lines).ToArray();
+            Assert.AreEqual(expectedResults, actualResults);
+        }
+    }
+}
