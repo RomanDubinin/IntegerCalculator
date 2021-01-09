f2a268f1bc8058e783f5d5a190615ce0b47bbf25
RomanDubinin <romandubinin94@gmail.com>
2021-01-09 09:25:41 +0100

    MainWindow.xaml.cs refactoring

--- CalculatorUI/MainWindow.xaml.cs
+++ CalculatorUI/MainWindow.xaml.cs
@@ -31,15 +31,17 @@ namespace CalculatorUI
             bunchCalculator = new BunchCalculator(calculator);
         }
 
+        #region GraphicalCalculator
+
         private void Digit_Click(object sender, RoutedEventArgs e)
         {
-            Button b = (Button) sender;
+            var b = (Button) sender;
             AddDigit(b.Content.ToString());
         }
 
         private void Operation_Click(object sender, RoutedEventArgs e)
         {
-            Button b = (Button) sender;
+            var b = (Button) sender;
             AddOperation(b.Content.ToString());
         }
 
@@ -55,30 +57,102 @@ namespace CalculatorUI
 
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
-            Delete();
+            ClearAll();
         }
 
         private void Key_Click(object sender, KeyEventArgs e)
         {
             //todo add oem keys
-            if (e.Key == Key.NumPad1) AddDigit("1");
-            if (e.Key == Key.NumPad2) AddDigit("2");
-            if (e.Key == Key.NumPad3) AddDigit("3");
-            if (e.Key == Key.NumPad4) AddDigit("4");
-            if (e.Key == Key.NumPad5) AddDigit("5");
-            if (e.Key == Key.NumPad6) AddDigit("6");
-            if (e.Key == Key.NumPad7) AddDigit("7");
-            if (e.Key == Key.NumPad8) AddDigit("8");
-            if (e.Key == Key.NumPad9) AddDigit("9");
-            if (e.Key == Key.NumPad0) AddDigit("0");
-            if (e.Key == Key.Add) AddOperation("+");
+            if (e.Key == Key.NumPad1)  AddDigit("1");
+            if (e.Key == Key.NumPad2)  AddDigit("2");
+            if (e.Key == Key.NumPad3)  AddDigit("3");
+            if (e.Key == Key.NumPad4)  AddDigit("4");
+            if (e.Key == Key.NumPad5)  AddDigit("5");
+            if (e.Key == Key.NumPad6)  AddDigit("6");
+            if (e.Key == Key.NumPad7)  AddDigit("7");
+            if (e.Key == Key.NumPad8)  AddDigit("8");
+            if (e.Key == Key.NumPad9)  AddDigit("9");
+            if (e.Key == Key.NumPad0)  AddDigit("0");
+            if (e.Key == Key.Add)      AddOperation("+");
             if (e.Key == Key.Subtract) AddOperation("-");
             if (e.Key == Key.Multiply) AddOperation("*");
-            if (e.Key == Key.Divide) AddOperation("/");
-            if (e.Key == Key.Return) CalculateAndShowResult();
-            if (e.Key == Key.Delete) Delete();
-            if (e.Key == Key.Back) RemoveLastCharacter();
+            if (e.Key == Key.Divide)   AddOperation("/");
+            if (e.Key == Key.Return)   CalculateAndShowResult();
+            if (e.Key == Key.Delete)   ClearAll();
+            if (e.Key == Key.Back)     RemoveLastCharacter();
         }
+
+        private void AddDigit(string digit)
+        {
+            expression.Append(digit);
+            ExpressionScreen.Text = expression.ToString();
+            ScrollToRightEnd();
+        }
+
+        private void AddOperation(string operation)
+        {
+            if (expression.Length > 0 && operations.Contains(expression[^1]))
+                expression.Remove(expression.Length-1, 1);
+
+            expression.Append(operation);
+            ExpressionScreen.Text = expression.ToString();
+            ScrollToRightEnd();
+        }
+
+        private async void CalculateAndShowResult()
+        {
+            if (expression.Length == 0)
+                return;
+
+            Screen.IsEnabled = false;
+            var calculationResult = await Task
+                                          .Run(() => calculator.CalculateFromString(expression.ToString()))
+                                          .ConfigureAwait(true);
+            Screen.IsEnabled = true;
+            expression.Clear();
+
+            if (!calculationResult.Validation.IsSuccess)
+            {
+                ExpressionScreen.Text += "\n" + new string(' ', calculationResult.Validation.ErrorPosition) + "^";
+                ErrorScreen.Text = calculationResult.Validation.ErrorMessage;
+                AnswerScreen.Text = "";
+            }
+            else if (calculationResult.CalculationError != null)
+            {
+                ErrorScreen.Text = calculationResult.CalculationError;
+                AnswerScreen.Text = "";
+            }
+            else
+            {
+                ErrorScreen.Text = "";
+                AnswerScreen.Text = calculationResult.Result.ToString();
+            }
+        }
+
+        private void ClearAll()
+        {
+            expression.Clear();
+            ExpressionScreen.Text = expression.ToString();
+        }
+
+        private void RemoveLastCharacter()
+        {
+            if (expression.Length > 0)
+                expression.Remove(expression.Length-1, 1);
+
+            ExpressionScreen.Text = expression.ToString();
+        }
+
+        private void ScrollToRightEnd()
+        {
+            //just because ScrollToEnd does not work
+            ExpressionScreen.ScrollToHorizontalOffset(double.MaxValue);
+        }
+
+
+        #endregion
+
+        #region BunchCalculation
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
@@ -113,53 +187,6 @@ namespace CalculatorUI
             }
         }
 
-        private void AddDigit(string digit)
-        {
-            expression.Append(digit);
-            ExpressionScreen.Text = expression.ToString();
-            ScrollToEnd();
-        }
-
-        private void AddOperation(string operation)
-        {
-            if (expression.Length > 0 && operations.Contains(expression[^1]))
-                expression.Remove(expression.Length-1, 1);
-
-            expression.Append(operation);
-            ExpressionScreen.Text = expression.ToString();
-            ScrollToEnd();
-        }
-
-        private async void CalculateAndShowResult()
-        {
-            if (expression.Length == 0)
-                return;
-
-            Screen.IsEnabled = false;
-            var calculationResult = await Task
-                                          .Run(() => calculator.CalculateFromString(expression.ToString()))
-                                          .ConfigureAwait(true);
-            Screen.IsEnabled = true;
-            expression.Clear();
-
-            if (!calculationResult.Validation.IsSuccess)
-            {
-                ExpressionScreen.Text += "\n" + new string(' ', calculationResult.Validation.ErrorPosition) + "^";
-                ErrorScreen.Text = calculationResult.Validation.ErrorMessage;
-                AnswerScreen.Text = "";
-            }
-            else if (calculationResult.CalculationError != null)
-            {
-                ErrorScreen.Text = calculationResult.CalculationError;
-                AnswerScreen.Text = "";
-            }
-            else
-            {
-                ErrorScreen.Text = "";
-                AnswerScreen.Text = calculationResult.Result.ToString();
-            }
-        }
-
         private void CalculateFile()
         {
             var expressions = File.ReadLines(fileToCalculate);
@@ -167,24 +194,6 @@ namespace CalculatorUI
             File.WriteAllLines(fileToSaveCalculations, results);
         }
 
-        private void Delete()
-        {
-            expression.Clear();
-            ExpressionScreen.Text = expression.ToString();
-        }
-
-        private void RemoveLastCharacter()
-        {
-            if (expression.Length > 0)
-                expression.Remove(expression.Length-1, 1);
-
-            ExpressionScreen.Text = expression.ToString();
-        }
-
-        private void ScrollToEnd()
-        {
-            //just because ScrollToEnd does not work
-            ExpressionScreen.ScrollToHorizontalOffset(double.MaxValue);
-        }
+        #endregion
     }
 }
